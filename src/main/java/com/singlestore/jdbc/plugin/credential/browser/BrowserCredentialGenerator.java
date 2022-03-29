ae73859597cecb198c35ea474f242bcb85b85234
hniemchenko <hniemchenko-ua@memsql.com>
2022-03-29 17:44:09 +0300

    server tests

--- src/main/java/com/singlestore/jdbc/plugin/credential/browser/BrowserCredentialGenerator.java
+++ src/main/java/com/singlestore/jdbc/plugin/credential/browser/BrowserCredentialGenerator.java
@@ -1,29 +1,9 @@
 package com.singlestore.jdbc.plugin.credential.browser;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTDecodeException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.exceptions.SignatureGenerationException;
-import com.auth0.jwt.exceptions.SignatureVerificationException;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.singlestore.jdbc.plugin.credential.Credential;
 import com.singlestore.jdbc.util.log.Logger;
 import com.singlestore.jdbc.util.log.Loggers;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.Properties;
-import java.util.Random;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.Collectors;
 
 public class BrowserCredentialGenerator {
   private static final Logger logger = Loggers.getLogger(BrowserCredentialGenerator.class);
@@ -39,139 +19,6 @@ public class BrowserCredentialGenerator {
       return server.WaitForCredential();
     } catch (InterruptedException e) {
       throw new SQLException("Interrupted while waiting for JWT", e);
-    }
-  }
-
-  private class TokenWaiterServer {
-    private final ReentrantLock credentialLock = new ReentrantLock();
-    private ExpiringCredential credential;
-    private final String listenPath;
-    private final HttpServer server;
-
-    TokenWaiterServer() throws SQLException {
-      try {
-        server = HttpServer.create(new InetSocketAddress(0), 0);
-      } catch (IOException e) {
-        throw new SQLException(
-            "Could not create a local HTTP server while using identity plugin 'BROWSER_SSO'", e);
-      }
-
-      listenPath = "/" + randomAlphanumeric(20);
-      server.createContext(listenPath, new RequestHandler());
-      credentialLock.lock();
-      server.start();
-    }
-
-    public ExpiringCredential WaitForCredential() throws InterruptedException {
-      credentialLock.wait();
-      server.stop(120);
-      return credential;
-    }
-
-    public String getListenPath() {
-      return listenPath;
-    }
-
-    public void setCredential(ExpiringCredential cred) {
-      credential = cred;
-      credentialLock.unlock();
-    }
-  }
-
-  // from https://www.baeldung.com/java-random-string 'Generate Random Alphanumeric String With Java
-  // 8'
-  private String randomAlphanumeric(int len) {
-    Random random = new Random();
-    return random
-        .ints(48, 123)
-        .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-        .limit(len)
-        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-        .toString();
-  }
-
-  private static class RequestHandler implements HttpHandler {
-    private TokenWaiterServer server;
-
-    public RequestHandler(TokenWaiterServer server) {
-      this.server = server;
-    }
-
-    public void handle(HttpExchange exchange) throws IOException {
-      if (!exchange.getRequestMethod().equals("POST")) {
-        error(exchange, 400, "POST expected");
-        return;
-      }
-
-      // TODO: check path
-
-      // read the whole response
-      String raw;
-      try {
-        raw =
-            new BufferedReader(new InputStreamReader(exchange.getRequestBody()))
-                .lines()
-                .parallel()
-                .collect(Collectors.joining("\n"));
-      } catch (Exception e) {
-        logger.debug("Bad read from request: ", e);
-        error(exchange, 500, "Bad read from request");
-        return;
-      }
-
-      DecodedJWT jwt;
-      try {
-        jwt = JWT.decode(raw);
-      } catch (JWTDecodeException e) {
-        logger.debug("Could not parse claims: ", e);
-        error(exchange, 400, "Could not parse claims: " + e.getMessage());
-        return;
-      }
-
-      JWTVerifier ver =
-          JWT.require(new DummyAlgorithm())
-              .withClaimPresence("email")
-              .withClaimPresence("dbUsername")
-              .build();
-      try {
-        ver.verify(jwt);
-      } catch (JWTVerificationException e) {
-        logger.debug("Could not verify claims: ", e);
-        error(exchange, 400, "Could not verify claims: " + e.getMessage());
-        return;
-      }
-
-      server.setCredential(
-          new ExpiringCredential(
-              new Credential(jwt.getClaim("dbUsername").asString(), jwt.getToken()),
-              jwt.getClaim("Email").asString(),
-              jwt.getExpiresAt().toInstant()));
-
-      exchange.sendResponseHeaders(204, -1);
-    }
-
-    private void error(HttpExchange exchange, int code, String errorMsg) throws IOException {
-      exchange.getResponseHeaders().set("Content-Type", "text/plain; charset=utf-8");
-      exchange.getResponseHeaders().set("X-Content-Type-Options", "nosniff");
-      exchange.sendResponseHeaders(code, 0);
-      exchange.getResponseBody().write(errorMsg.getBytes(StandardCharsets.UTF_8));
-      exchange.getResponseBody().close();
-    }
-  }
-
-  private static class DummyAlgorithm extends Algorithm {
-    public DummyAlgorithm() {
-      super(
-          "DummyAlgorithm",
-          "Does not do any signature verification. Used to only verify claims for a token");
-    }
-
-    @Override
-    public void verify(DecodedJWT decodedJWT) throws SignatureVerificationException {}
-
-    @Override
-    public byte[] sign(byte[] bytes) throws SignatureGenerationException {
-      return null;
     }
   }
 }
---
+++ src/test/java/com/singlestore/jdbc/unit/plugin/BrowserAuthTest.java
@@ -0,0 +1,142 @@
+package com.singlestore.jdbc.unit.plugin;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.singlestore.jdbc.plugin.credential.browser.ExpiringCredential;
+import com.singlestore.jdbc.plugin.credential.browser.TokenWaiterServer;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.Instant;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.jupiter.api.Test;
+
+public class BrowserAuthTest {
+
+  @Test
+  public void TokenWaiterServerTest() throws SQLException, IOException, InterruptedException {
+    /*{
+      "email": "test-email@gmail.com",
+      "dbUsername": "test-user",
+      "exp": 1916239022 (year 2030)
+    }*/
+    String jwt =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QtZW1haWxAZ21haWwuY29tIiwiZGJVc2VybmFtZS"
+            + "I6InRlc3QtdXNlciIsImV4cCI6MTkxNjIzOTAyMn0.kQPJ2yLs8-G5bUuYBddmyKGQmaimVop2mptZ5IqtF3c";
+    TokenWaiterServer server = new TokenWaiterServer();
+    String path = server.getListenPath();
+    HttpClient httpclient = HttpClients.createDefault();
+    HttpPost httppost = new HttpPost("http:/" + path);
+
+    StringEntity entity = new StringEntity(jwt);
+    httppost.setEntity(entity);
+    HttpResponse response = httpclient.execute(httppost);
+
+    assertEquals(response.getStatusLine().getStatusCode(), 204);
+    assertNull(response.getEntity());
+    ExpiringCredential credential = server.WaitForCredential();
+    assertEquals("test-email@gmail.com", credential.getEmail());
+    assertEquals("test-user", credential.getCredential().getUser());
+    assertEquals(jwt, credential.getCredential().getPassword());
+    assertEquals(Instant.ofEpochSecond(1916239022), credential.getExpiration());
+  }
+
+  @Test
+  public void TokenWaiterServerErrorsTest() throws SQLException, IOException {
+    TokenWaiterServer server = new TokenWaiterServer();
+    String path = server.getListenPath();
+    HttpClient httpclient = HttpClients.createDefault();
+
+    // non-POST request
+    HttpGet httpGet = new HttpGet("http:/" + path);
+    HttpResponse response = httpclient.execute(httpGet);
+    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+    assertNotNull(response.getEntity());
+    response.getEntity().writeTo(stream);
+    assertEquals("POST expected", stream.toString());
+    assertEquals(400, response.getStatusLine().getStatusCode());
+
+    // invalid JWT
+    String jwt = "wrong-jwt";
+    HttpPost httppost = new HttpPost("http:/" + path);
+
+    StringEntity entity = new StringEntity(jwt);
+    httppost.setEntity(entity);
+    response = httpclient.execute(httppost);
+    stream = new ByteArrayOutputStream();
+    assertNotNull(response.getEntity());
+    response.getEntity().writeTo(stream);
+    assertEquals(
+        "Could not parse claims: The token was expected to have 3 parts, but got 1.",
+        stream.toString());
+    assertEquals(400, response.getStatusLine().getStatusCode());
+
+    // missing JWT claims
+    /*{
+      "not-email": "test-email@gmail.com",
+      "dbUsername": "test-user",
+      "exp": 1916239022
+    }*/
+    jwt =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub3QtZW1haWwiOiJ0ZXN0LWVtYWlsQGdtYWlsLmNvbSIsImRiVXNlcm5hb"
+            + "WUiOiJ0ZXN0LXVzZXIiLCJleHAiOjE5MTYyMzkwMjJ9.B4Ry70_6jDvK0n-qEzAnihgsKnL0PivYbPY2BLSandc";
+    httppost = new HttpPost("http:/" + path);
+
+    entity = new StringEntity(jwt);
+    httppost.setEntity(entity);
+    response = httpclient.execute(httppost);
+    stream = new ByteArrayOutputStream();
+    assertNotNull(response.getEntity());
+    response.getEntity().writeTo(stream);
+    assertEquals(
+        "Could not verify claims: The Claim 'email' is not present in the JWT.", stream.toString());
+    assertEquals(400, response.getStatusLine().getStatusCode());
+
+    // expired JWT
+    /*{
+      "email": "test-email@gmail.com",
+      "dbUsername": "test-user",
+      "exp": 1000000000
+    }*/
+    jwt =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QtZW1haWxAZ21haWwuY29tIiwiZGJVc2VybmFtZSI6In"
+            + "Rlc3QtdXNlciIsImV4cCI6MTAwMDAwMDAwMH0.yemofu_U02SMEtVg8Mu7XJ9J8clbCuhdacAR0jWY4X0";
+    httppost = new HttpPost("http:/" + path);
+
+    entity = new StringEntity(jwt);
+    httppost.setEntity(entity);
+    response = httpclient.execute(httppost);
+    stream = new ByteArrayOutputStream();
+    assertNotNull(response.getEntity());
+    response.getEntity().writeTo(stream);
+    assertEquals(
+        "Could not verify claims: The Token has expired on Sun Sep 09 04:46:40 EEST 2001.",
+        stream.toString());
+    assertEquals(400, response.getStatusLine().getStatusCode());
+
+    // no expiration JWT
+    /*{
+      "email": "test-email@gmail.com",
+      "dbUsername": "test-user",
+    }*/
+    jwt =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QtZW1haWxAZ21haWwuY29tIiwiZGJVc2VybmFtZSI6In"
+            + "Rlc3QtdXNlciJ9.KqJ5aJVDK08y91zrUvlpV3pONgNgYE1iNRRtiUS5HWk";
+    httppost = new HttpPost("http:/" + path);
+
+    entity = new StringEntity(jwt);
+    httppost.setEntity(entity);
+    response = httpclient.execute(httppost);
+    stream = new ByteArrayOutputStream();
+    assertNotNull(response.getEntity());
+    response.getEntity().writeTo(stream);
+    assertEquals(
+        "Could not verify claims: The Claim 'exp' is not present in the JWT.", stream.toString());
+    assertEquals(400, response.getStatusLine().getStatusCode());
+  }
+}
