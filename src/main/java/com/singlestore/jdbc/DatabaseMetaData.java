1a4b5e352a61de1aba4f13366bf003599e53d7c8
joeljons <joel@dbvis.com>
2024-09-10 22:12:17 +0200

    Avoid ClassCastException when calling getDatabaseProductVersion using a connection with defaultFetchSize

--- src/main/java/com/singlestore/jdbc/DatabaseMetaData.java
+++ src/main/java/com/singlestore/jdbc/DatabaseMetaData.java
@@ -7,6 +7,7 @@ package com.singlestore.jdbc;
 
 import com.singlestore.jdbc.client.DataType;
 import com.singlestore.jdbc.client.result.CompleteResult;
+import com.singlestore.jdbc.client.result.StreamingResult;
 import com.singlestore.jdbc.util.Version;
 import com.singlestore.jdbc.util.VersionFactory;
 import java.sql.PseudoColumnUsage;
@@ -171,9 +172,13 @@ public class DatabaseMetaData implements java.sql.DatabaseMetaData {
   private ResultSet executeQuery(String sql) throws SQLException {
     Statement stmt =
         connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-    CompleteResult rs = (CompleteResult) stmt.executeQuery(sql);
-    rs.setStatement(null); // bypass Hibernate statement tracking (CONJ-49)
-    rs.useAliasAsName();
+    ResultSet rs = stmt.executeQuery(sql);
+    if (rs instanceof CompleteResult) {
+      ((CompleteResult) rs).setStatement(null); // bypass Hibernate statement tracking (CONJ-49)
+      ((CompleteResult) rs).useAliasAsName();
+    } else if (rs instanceof StreamingResult) {
+      ((StreamingResult) rs).setStatement(null); // bypass Hibernate statement tracking (CONJ-49)
+    }
     return rs;
   }
 
